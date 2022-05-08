c7b34488546ddebfa942250cdd91bc91337059ae
Srikanth Reddy <srikanth131096@gmail.com>
2022-05-08 16:20:43 +0530

    Modify user interaction

--- rps_logic.js
+++ rps_logic.js
@@ -1,50 +1,57 @@
-let playerScore = 0
-let computerScore = 0
+let playerScore = 0;
+let computerScore = 0;
+let computerSelection;
+let playerSelection;
+const options = ["rock","paper","scissors"];
+
 
 // create function to randomly pick 
 function computerPlay(){
-    const options = ["rock","paper","scissors"]
-    const computerSelection = options[Math.floor(Math.random()*3)]
-    console.log(computerSelection)
+    computerSelection = options[Math.floor(Math.random()*3)]
     return computerSelection
 }
 
 // create function to compare player selection with computer selection
 function playRound(playerSelection,computerSelection){
-    playerSelection = playerSelection.toLowerCase()
+    computerSelection = computerPlay();
     if(playerSelection === computerSelection){
+        computerScore += 1
+        playerScore += 1
         return "It's a tie!";
-    }else if(playerSelection == "rock" && computerSelection == "paper"){
-        computerScore += 1;
-        return "You Lose! Paper beats Rock";
-    }else if(playerSelection == "rock" && computerSelection == "scissors"){
-        playerScore += 1;
-        return "You Win! Rock beats Scissors";
-    }else if(playerSelection == "paper" && computerSelection == "rock"){
-        playerScore += 1;
-        return "You Win! Paper beats Rock";
-    }else if(playerSelection == "paper" && computerSelection == "scissors"){
-        computerScore += 1;
-        return "You Lose! Scissors beats Paper";
-    }else if(playerSelection == "scissors" && computerSelection == "rock"){
-        computerScore += 1;
-        return "You Lose! Rock beats Scissors";
-    }else if(playerSelection == "scissors" && computerSelection == "paper"){
-        playerScore += 1;
-        return "You Win! Scissors beats Paper";
+    }else if((playerSelection == "rock" && computerSelection == "paper") ||
+             (playerSelection == "paper" && computerSelection == "scissors") ||
+             (playerSelection == "scissors" && computerSelection == "rock")) {
+        computerScore += 1
+        return "You Lose!";
+    }else if((playerSelection == "rock" && computerSelection == "scissors") || 
+             (playerSelection == "paper" && computerSelection == "rock") ||
+             (playerSelection == "scissors" && computerSelection == "paper")) {
+        playerScore += 1
+        return "You Win!";
     }else{
         return "Please input valid option";
     }
 }
 
-// create function to play best of five rounds
-function game(){
-    for(let round=0; round<5; round++){
-        const computerSelection = computerPlay()
-        const playerSelection = prompt("Enter your selection(Rock, Paper, Scissors): ") // take input from user
-        console.log(playRound(playerSelection,computerSelection))
-    }
-    return (playerScore>computerScore)?"You won the series!!!":(playerScore==computerScore)?"Series tied!!!":"You lost the series, better luck next time."
-    
+function declareResult() {
+    finalResult.textContent = (playerScore>computerScore)?"You won the series!!!":
+                (playerScore==computerScore)?"Series tied!!!":"You lost the series, better luck next time.";
+    body.appendChild(finalResult);
 }
-console.log(game())
+
+
+let buttons = document.querySelectorAll('button');
+let body = document.querySelector('body');
+let result = document.createElement('p');
+let finalResult = document.createElement('p');
+buttons.forEach(button => {
+    button.addEventListener('click', (e) => {
+        playerSelection = e.target.id;
+        result.textContent = playRound(playerSelection,computerSelection);
+        body.appendChild(result);
+        console.log(playerScore+ ' '+computerScore);
+        if (playerScore == 5 || computerScore == 5){
+            declareResult();
+        }
+    })
+})
