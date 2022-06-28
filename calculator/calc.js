13b607244522d3a299170d5cf7816dc09f396b6d
Srikanth Reddy <srikanth131096@gmail.com>
2022-06-28 21:37:02 +0530

    Update files to take input from UI

--- calculator/calc.js
+++ calculator/calc.js
@@ -1,5 +1,91 @@
+let firstOperand = ''
+let secondOperand = ''
+let currentOperation = null
+let shouldResetScreen = false
+
+const numberButtons = document.querySelectorAll('[data-number]')
+const operatorButtons = document.querySelectorAll('[data-operator]')
+const equalsButton = document.getElementById('equals')
+const decimalButton = document.getElementById('decimal')
+const deleteButton = document.getElementById('delete')
+const clearButton = document.getElementById('clear')
+const lastOperationScreen = document.getElementById('lastOperationScreen')
+const currentOperationScreen = document.getElementById('currentOperationScreen')
+
+equalsButton.addEventListener('click', evaluate)
+clearButton.addEventListener('click', clear)
+deleteButton.addEventListener('click', deleteNumber)
+decimalButton.addEventListener('click', appendPoint)
+
+numberButtons.forEach((button) => 
+    button.addEventListener('click', () => appendNumber(button.textContent))
+  )
+
+operatorButtons.forEach((button) =>
+    button.addEventListener('click', () => setOperation(button.textContent))
+)
+
+
+function appendNumber(number) {
+  if (currentOperationScreen.textContent === '0' || shouldResetScreen) resetScreen()
+  currentOperationScreen.textContent += number
+}
+
+function resetScreen() {
+  currentOperationScreen.textContent = ''
+  shouldResetScreen = false
+}
+
+function clear() {
+  currentOperationScreen.textContent = '0'
+  lastOperationScreen.textContent = ''
+  firstOperand = ''
+  secondOperand = ''
+  currentOperation = null
+}
+
+function appendPoint() {
+  if (shouldResetScreen) resetScreen()
+  if (currentOperationScreen.textContent === '')
+    currentOperationScreen.textContent = '0'
+  if (currentOperationScreen.textContent.includes('.')) return
+  currentOperationScreen.textContent += '.'
+}
+
+function deleteNumber() {
+  currentOperationScreen.textContent = currentOperationScreen.textContent
+    .toString()
+    .slice(0, -1)
+}
+
+function setOperation(operator) {
+  if (currentOperation !== null) evaluate()
+  firstOperand = currentOperationScreen.textContent
+  currentOperation = operator
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`
+  shouldResetScreen = true
+}
+
+function evaluate() {
+  if (currentOperation === null || shouldResetScreen) return
+  if (currentOperation === '÷' && currentOperationScreen.textContent === '0') {
+    alert("You can't divide by 0!")
+    return
+  }
+  secondOperand = currentOperationScreen.textContent
+  currentOperationScreen.textContent = roundResult(
+    operate(currentOperation, firstOperand, secondOperand)
+  )
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`
+  currentOperation = null
+}
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000
+}
+
 const add = function(a,b) {
-    return a+b;
+  return a+b;
 };
 
 const subtract = function(a,b) {
@@ -7,9 +93,26 @@ const subtract = function(a,b) {
 };
 
 const multiply = function(a,b) {
-    return a*b;
+  return a*b;
 };
 
 const divide = function(a,b) {
-    return a/b;
+  return a/b;
 };
+
+const operate = function(operator, a, b) {
+  a = Number(a)
+  b = Number(b)
+  switch(operator) {
+    case '+':
+      return add(a,b);
+    case '-':
+      return subtract(a,b);
+    case 'x':
+      return multiply(a,b);
+    case '÷':
+      return divide(a,b);
+    default:
+      return null;
+  }
+}
