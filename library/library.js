1a5fab4c13426e3204f4c97fb05ace659dace31f
Srikanth Reddy <srikanth131096@gmail.com>
2022-08-26 23:49:30 +0530

    Create library skeleton

---
+++ library/library.js
@@ -0,0 +1,61 @@
+let myLibrary = [
+    {
+        name: "defaultBook",
+        author: "defaultAuthor",
+        read: "Read"
+    }
+];
+
+function displayLibrary(){
+    myLibrary.forEach(book => {
+        addBook(book.name, book.author, book.read)
+    })
+}
+
+function updateLibrary() {
+    let bookDetails = {}
+    const bookName = document.querySelector('#name').value
+    const bookAuthor = document.querySelector('#author').value
+    bookDetails.name = bookName
+    bookDetails.author = bookAuthor
+    bookDetails.read = "Read"
+    myLibrary.push(bookDetails)
+    addBook(bookDetails.name, bookDetails.author, bookDetails.read)
+    return bookDetails
+}
+
+function addBook(name, author, read){
+    const book = document.createElement("div")
+    const bookName = document.createElement("h2")
+    const bookAuthor = document.createElement("h3")
+    const bookRead = document.createElement("button")
+    book.className = "book"
+    book.appendChild(bookName)
+    book.appendChild(bookAuthor)
+    book.appendChild(bookRead)
+    bookName.className = "b_title"
+    bookName.innerText = name
+    bookAuthor.className = "b_author"
+    bookAuthor.innerText = author
+    bookRead.className = "b_read"
+    bookRead.innerHTML = read
+    books.appendChild(book)
+}
+
+function changeBackground(element){
+    element.classList.toggle("not_read")
+}
+
+const books = document.querySelector('.books')
+displayLibrary()
+const addBookBtn = document.querySelector('#addBookBtn')
+// const detail = addBookBtn.addEventListener('click', updateLibrary)
+const detailNew = addBookBtn.addEventListener('click', (event) => {
+    updateLibrary()
+    let readStatus = document.querySelectorAll('.b_read')
+    console.log(readStatus)
+    readStatus.forEach(button => {
+        button.addEventListener("click", () => changeBackground(button.parentElement))
+        }
+    )
+})
