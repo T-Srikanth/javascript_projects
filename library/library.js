dbd6b285f008c67ab738f3dee3e7ca13f2edcab3
Srikanth Reddy <srikanth131096@gmail.com>
2022-08-28 21:21:10 +0530

    Create modal form structure and add book delete fuctionality

--- library/library.js
+++ library/library.js
@@ -29,25 +29,45 @@ function addBook(name, author, read){
     const bookName = document.createElement("h2")
     const bookAuthor = document.createElement("h3")
     const bookRead = document.createElement("button")
+    const bookDelete =document.createElement("button")
     book.className = "book"
     book.appendChild(bookName)
     book.appendChild(bookAuthor)
     book.appendChild(bookRead)
+    book.appendChild(bookDelete)
     bookName.className = "b_title"
     bookName.innerText = name
     bookAuthor.className = "b_author"
     bookAuthor.innerText = author
     bookRead.className = "b_read"
-    bookRead.innerHTML = read
+    bookRead.innerText = read
+    bookDelete.className = "b_delete"
+    bookDelete.innerText = "Delete"
     books.appendChild(book)
     bookRead.addEventListener("click", () => changeBackground(bookRead.parentElement))
+    bookDelete.addEventListener("click", () => deleteBook(bookDelete.parentElement))
+}
+
+function deleteBook(element){
+    element.remove()
 }
 
 function changeBackground(element){
     element.classList.toggle("not_read")
 }
 
+function displayModal(){
+    modalForm.style.display = "block";
+}
+
 const books = document.querySelector('.books')
-displayLibrary()
 const addBookBtn = document.querySelector('#addBookBtn')
-const detail = addBookBtn.addEventListener('click', updateLibrary)
+const modalForm = document.querySelector('.modal_form')
+addBookBtn.addEventListener('click', displayModal)
+displayLibrary()
+const submit = document.querySelector('#submit')
+const detail = submit.addEventListener('click', () =>{
+    updateLibrary()
+    modalForm.style.display = "none"
+    }
+)
